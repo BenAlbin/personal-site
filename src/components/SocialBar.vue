c8cadece0ac25001e7f62872ccd40cc1b49a7ab2
Ben Albin <ben.g.albin@gmail.com>
2018-10-10 16:27:15 +0100

    Move data into json file

--- src/components/SocialBar.vue
+++ src/components/SocialBar.vue
@@ -2,7 +2,7 @@
   <v-card>
     <v-card-actions>
       <social-button
-        v-for="(social, index) in socials"
+        v-for="(social, index) in $options.socials"
         v-bind:socialUrl="social.socialUrl"
         v-bind:socialIcon="social.socialIcon"
         v-bind:key="index"
@@ -13,21 +13,13 @@
 
 <script>
 import SocialButton from './SocialButton.vue'
+import socialsData from '@/data/Socials.json'
 
 export default {
   name: 'SocialBar',
+  socials: socialsData,
   data () {
     return {
-      socials: [
-        {
-          socialUrl: "https://github.com/BenAlbin",
-          socialIcon: "github"
-        },
-        {
-          socialUrl: "https://www.linkedin.com/in/ben-albin-ab788673/",
-          socialIcon: "linkedin"
-        }
-      ]
     }
   },
   components: {
